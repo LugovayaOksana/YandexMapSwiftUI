a95466984be215ea0b936ea152dcb85582abc288
Оксана Луговая <otl@fivegen.ru>
2023-05-22 19:58:26 +0300

    Initial Commit

---
+++ YandexMap/YandexMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexMapApp.swift
+//  YandexMap
+//
+//  Created by Оксана on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct YandexMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
